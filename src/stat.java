ba902dadbd149b69cb7f2afa4c3401191a581eea
Ksenobait <Strelok-13@mail.ru>
2013-02-22 21:48:00 +0700

    first commit

---
+++ src/stat.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Администратор
+ * Date: 22.02.13
+ * Time: 22:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class stat {
+    public static void main(String[] args){
+        System.out.println("sdfghjk");
+    }
+}
