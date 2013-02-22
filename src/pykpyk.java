11cbca1a135798c60988c6977d952565e95c0d12
Ksenobait <Strelok-13@mail.ru>
2013-02-22 21:51:46 +0700

    test

---
+++ src/pykpyk.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Администратор
+ * Date: 22.02.13
+ * Time: 22:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class pykpyk {
+}
